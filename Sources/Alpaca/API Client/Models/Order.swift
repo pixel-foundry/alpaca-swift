4fde5e3b4488249c9bf68f899f3577907c563459
Hal Lee <hal@lee.me>
2020-05-10 06:57:14 -0400

    Manually implement Order `Decodable`

--- Sources/Alpaca/API Client/Models/Order.swift
+++ Sources/Alpaca/API Client/Models/Order.swift
@@ -1,6 +1,6 @@
 import Foundation
 
-public struct Order: Codable, Hashable {
+public struct Order: Hashable {
 
 	public let id: String
 	/// Client unique order ID.
@@ -38,11 +38,96 @@ public struct Order: Codable, Hashable {
 	/// an array of Order entities associated with this order. Otherwise, null.
 	public let legs: [Order]?
 
+	public enum CodingKeys: String, CodingKey {
+		case id
+		case clientOrderID = "clientOrderId"
+		case createdAt
+		case updatedAt
+		case submittedAt
+		case filledAt
+		case expiredAt
+		case canceledAt
+		case failedAt
+		case replacedAt
+		case replacedBy
+		case replaces
+		case assetID = "assetId"
+		case symbol
+		case assetClass
+		case qty
+		case filledQty
+		case type
+		case side
+		case timeInForce
+		case limitPrice
+		case stopPrice
+		case filledAvgPrice
+		case status
+		case extendedHours
+		case legs
+	}
+
 }
+
+extension Order: Encodable { }
+
+extension Order: Decodable {
+
+	public init(from decoder: Decoder) throws {
+		let values = try decoder.container(keyedBy: CodingKeys.self)
+		id = try values.decode(String.self, forKey: .id)
+		clientOrderID = try values.decode(String.self, forKey: .clientOrderID)
+		createdAt = try values.decode(Date.self, forKey: .createdAt)
+		updatedAt = try values.decode(Date?.self, forKey: .updatedAt)
+		submittedAt = try values.decode(Date?.self, forKey: .submittedAt)
+		filledAt = try values.decode(Date?.self, forKey: .filledAt)
+		expiredAt = try values.decode(Date?.self, forKey: .expiredAt)
+		canceledAt = try values.decode(Date?.self, forKey: .canceledAt)
+		failedAt = try values.decode(Date?.self, forKey: .failedAt)
+		replacedAt = try values.decode(Date?.self, forKey: .replacedAt)
+		replacedBy = try values.decode(String?.self, forKey: .replacedBy)
+		replaces = try values.decode(String?.self, forKey: .replaces)
+		assetID = try values.decode(String.self, forKey: .assetID)
+		symbol = try values.decode(String.self, forKey: .symbol)
+		assetClass = try values.decode(String.self, forKey: .assetClass)
+		let quantityString = try values.decode(String.self, forKey: .qty)
+		guard let quantity = Int(quantityString) else { throw OrderDecodeError.invalidQuantity }
+		qty = quantity
+		let filledQuantityString = try values.decode(String.self, forKey: .filledQty)
+		guard let filledQuantity = Int(filledQuantityString) else { throw OrderDecodeError.invalidQuantity }
+		filledQty = filledQuantity
+		type = try values.decode(OrderType.self, forKey: .type)
+		side = try values.decode(OrderSide.self, forKey: .side)
+		timeInForce = try values.decode(TimeInForce.self, forKey: .timeInForce)
+
+		let limitString = try values.decode(String?.self, forKey: .limitPrice)
+		let limitDecimal = Decimal(string: limitString ?? "")
+		limitPrice = limitDecimal
+
+		let stopString = try values.decode(String?.self, forKey: .stopPrice)
+		let stopDecimal = Decimal(string: stopString ?? "")
+		stopPrice = stopDecimal
+
+		let filledString = try values.decode(String?.self, forKey: .filledAvgPrice)
+		let filledDecimal = Decimal(string: filledString ?? "")
+		filledAvgPrice = filledDecimal
+
+		status = try values.decode(OrderStatus.self, forKey: .status)
+		extendedHours = try values.decode(Bool.self, forKey: .extendedHours)
+		legs = try values.decode([Order]?.self, forKey: .legs)
+	}
+
+	enum OrderDecodeError: Error {
+		case invalidQuantity
+	}
+
+}
+
 
 @available(OSX 10.15, iOS 13, tvOS 13, watchOS 6, *)
 extension Order: Identifiable { }
 
+/// See [Alpaca’s docs](https://alpaca.markets/docs/trading-on-alpaca/orders/#order-lifecycle) for more details.
 public enum OrderStatus: String, Codable, Hashable {
 	case new
 	case partiallyFilled
@@ -53,4 +138,11 @@ public enum OrderStatus: String, Codable, Hashable {
 	case replaced
 	case pendingCancel
 	case pendingReplace
+	case accepted
+	case pendingNew
+	case acceptedForBidding
+	case stopped
+	case rejected
+	case suspended
+	case calculated
 }
